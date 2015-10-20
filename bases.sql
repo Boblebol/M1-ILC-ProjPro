89402be26ac029c974c4610a7e58412461c4feb1
Boblebol <alexandre.enouf@gmail.com>
2015-10-20 18:41:32 +0200

    Update bases.sql

--- bases.sql
+++ bases.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Prommotion
+CREATE TABLE Prommotions
 (
     -- LA PROMOTION
-    id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- id de la promotion
+    idPromo         INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- id de la promotion
     nomPromo        VARCHAR(50), -- nom de la promo
     -- LE PRODUIT
     nomProduit      VARCHAR(50), -- nom/ref du produit
@@ -11,6 +11,7 @@ CREATE TABLE Prommotion
     ancienPrix      INT, -- ancien prix du produit
     NouveauPrix     INT, -- nouveau prix du produit
     estimationSt    VARCHAR(50), --estimation du stock
+    dureeValidite   INT, --duree de validite de la promo -> trouver meilleur type, peut etre date 
     -- LE MAGASIN
     magasin         VARCHAR(50), -- nom du magasin
     numRue          INT NOT NULL, -- addresse
@@ -18,5 +19,11 @@ CREATE TABLE Prommotion
     codePostal      INT(5) NOT NULL, -- addresse
     ville           VARCHAR(50) NOT NULL, -- addresse
     -- LE CREATEUR
-    idUser          INT NOT NULL FOREIGN KEY REFERENCES Users(id)
+    idUser          INT NOT NULL FOREIGN KEY REFERENCES Utilisateurs(id)
+)
+
+CREATE TABLE Utilisateurs
+(
+	--L'utilisateur
+	idUtilisateur	INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 )
