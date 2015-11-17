36ac1e0d25734e101352f7f2ef33255e67fbad75
XWing <XWing@MacBook-Air-de-Enouf.local>
2015-11-17 12:30:40 +0100

    Evolution du War

--- Serveur/Bases/tablePromotion.sql
+++ Serveur/Bases/tablePromotion.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Prommotions
+CREATE TABLE PrommotionsA
 (
 	 -- LA PROMOTION
 	 idPromo		INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- id de la promotion
@@ -8,15 +8,11 @@ CREATE TABLE Prommotions
 	 marque          	VARCHAR(50), -- marque du produit
 	 catégorie       	     VARCHAR(50), -- catégorie du produit -> les imposer dans le formulaire d'ajout de promo
 	 description     	       BLOB, -- description du produit
- 	 ancienPrix      	    INT, -- ancien prix du produit
-	 nouveauPrix      INT, -- nouveau prix du produit
+ 	 ancienPrix      	    FLOAT, -- ancien prix du produit
+	 nouveauPrix      FLOAT, -- nouveau prix du produit
 	 estimationStock      VARCHAR(50), -- estimation du stock
 	 dateCreation     TIMESTAMP, -- date ou la promo a ete postee
-<<<<<<< HEAD:Bases/tablePromotion.sql
 	 dureeValidite   	       INT,
-=======
-	 dureeValidite   	     	INT,
->>>>>>> origin/master:Serveur/Bases/tablePromotion.sql
 	 -- LE MAGASIN
 	 magasin		VARCHAR(50), -- nom du magasin
 	 -- LOCALISATION POSTALE
@@ -25,8 +21,8 @@ CREATE TABLE Prommotions
 	 codePostal      		INT(5) NOT NULL, -- addresse
 	 ville           	VARCHAR(50) NOT NULL, -- addresse
 	 -- LOCALISTAION SATELLITE
-	 longitude		INT NOT NULL, -- longitude
-	 lattitude       	INT NOT NULL, -- lattitude
+	 longitude		FLOAT NOT NULL, -- longitude
+	 lattitude       	FLOAT NOT NULL, -- lattitude
 	 -- STATUT
 	 active			BOOLEAN, -- Tant que la promo as moins de 24h par defaut ou que 3 personne l'aient signalee
 	 -- LE CREATEUR
