c260b8772e9c8e2c4b861b541173bc927b829d8e
boblebol <alexandre.enouf@gmail.com>
2016-03-15 18:07:17 +0100

    tables de données
    modifications mineures

--- Serveur/Bases/tableAssociationUtilisateurPromotion.sql
+++ Serveur/Bases/tableAssociationUtilisateurPromotion.sql
@@ -7,16 +7,9 @@ CREATE TABLE AssociationUtilisateurPromotion
 	idClient		INT NOT NULL,
 	-- LE SUIVI
 	marque		VARCHAR(50), -- marque du produit
-<<<<<<< HEAD
 	catégorie              VARCHAR(50), -- catégorie du produit
 	magasin 	INT NOT NULL, -- un magasin
 	FOREIGN KEY (idClient)REFERENCES Client(idClient), -- id de l'utilisateur
 	FOREIGN KEY (magasin) REFERENCES Magasin(idMagasin) -- un magasin
-=======
-	categorie              VARCHAR(50), -- catégorie du produit
-	idUtilSuivi	INT NOT NULL,
-	FOREIGN KEY (idUtilisateur)REFERENCES Utilisateurs(idUtilisateur), -- id de l'utilisateur
-	FOREIGN KEY (idUtilSuivi) REFERENCES Utilisateurs(idUtilisateur) -- un autre utilisateur
->>>>>>> origin/master
 )
 	  
--- Serveur/Bases/tablePromotion.sql
+++ Serveur/Bases/tablePromotion.sql
@@ -1,35 +1,18 @@
-<<<<<<< HEAD
-CREATE TABLE Prommotions
-=======
+
 CREATE TABLE Promotions
->>>>>>> origin/master
 (
 	 -- LA PROMOTION
 	 idPromo			INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- id de la promotion
 	 referencePromo     VARCHAR(50), -- nom de la promo
 	 -- LE PRODUIT
-<<<<<<< HEAD
 	 marquePromo        VARCHAR(50), -- marque du produit
 	 nomProduit			VARCHAR(50), -- marque du produit
-	 catégorie       	     VARCHAR(50), -- catégorie du produit -> les imposer dans le formulaire d'ajout de promo
-=======
-	 nomProduit		VARCHAR(50), -- nom/ref du produit
-	 marque          	VARCHAR(50), -- marque du produit
 	 categorie       	     VARCHAR(50), -- catégorie du produit -> les imposer dans le formulaire d'ajout de promo
->>>>>>> origin/master
 	 description     	       BLOB, -- description du produit
  	 ancienPrix      	    FLOAT, -- ancien prix du produit
 	 nouveauPrix      FLOAT, -- nouveau prix du produit
 	 dateCreation     TIMESTAMP, -- date ou la promo a ete postee
 	 dureeValidite   	       INT,
-<<<<<<< HEAD
-=======
-	 -- LE MAGASIN
-	 magasin		VARCHAR(50), -- nom du magasin
-	 -- LOCALISTAION SATELLITE
-	 longitude		FLOAT NOT NULL, -- longitude
-	 latitude       	FLOAT NOT NULL, -- lattitude
->>>>>>> origin/master
 	 -- STATUT
 	 active			BOOLEAN, -- Tant que la promo as moins de 24h par defaut ou que 3 personne l'aient signalee
 	 -- LE MAGASIN qui a créée l'annonce
