6b3b32f6b9dde84fe42f4a20d958c6336060da52
Genkidama <tom.gimenes913@gmail.com>
2016-04-08 11:17:10 +0200

    MAJ

--- Destock/demo/www/app/connexion/connexion.ctrl.js
+++ Destock/demo/www/app/connexion/connexion.ctrl.js
@@ -1,6 +1,42 @@
 angular.module('demo.connexion.ctrl', [])
 
-  .controller('ConnexionCtrl', function ($scope) {
+  .controller('ConnexionCtrl', function ($scope, $http) {
 
+   $scope.SendData = function () {
+   
+   
+   
+           // use $.param jQuery function to serialize data from JSON 
+            var dataOBJ = {
+                mail: $scope.mail,
+                mdp: $scope.mdp
+            };
+        
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
 
+                $scope.Donnees = dataOBJ;
+                   
+            $http.post('http://destock.u-strasbg.fr:8080/Destock/client/con	', dataOBJ, config)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    " Status: " + status;
+            });
+            
+            
+            
+            
+        };
+        
+        
   });
+  
+ 
+        
+//action="http://destock.u-strasbg.fr:8080/Destock/client/add"
