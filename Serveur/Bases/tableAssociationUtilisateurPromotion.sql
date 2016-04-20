e69866a5bf022e077f6c5ad9018f2330470fc021
Boblebol <alexandre.enouf@gmail.com>
2016-04-20 22:45:52 +0200

    Retrait de clef etrangère

--- Serveur/Bases/tableAssociationUtilisateurPromotion.sql
+++ Serveur/Bases/tableAssociationUtilisateurPromotion.sql
@@ -8,8 +8,7 @@ CREATE TABLE AssociationUtilisateurPromotion
 	-- LE SUIVI
 	marque		VARCHAR(50), -- marque du produit
 	categorie              VARCHAR(50), -- catégorie du produit
-	magasin 	INT, -- un magasin
+	idMagasin 	INT, -- un magasin
 	FOREIGN KEY (idClient)REFERENCES Client(idClient), -- id de l'utilisateur
-	FOREIGN KEY (magasin) REFERENCES Magasin(idMagasin) -- un magasin
 )
 	  
