171a48dcce4f66fc90cc6be84e6029e5c6ae3a0f
Minipera <olivierperardelle@gmail.com>
2016-04-23 14:35:24 +0200

    Mise en place de la connexion au serveur avec les Offres, Recuperation du JSON, affichage au cas par cas et Champ recherche opérationnel

--- Destock/demo/www/app/offres/offres.ctrl.js
+++ Destock/demo/www/app/offres/offres.ctrl.js
@@ -1,57 +1,43 @@
 angular.module('demo.offres.ctrl', [])
 
-  .controller('OffresCtrl', function ($scope) {
+  .controller('OffresCtrl', function ($scope, $http,$httpParamSerializer) {
+  
+  
+   $scope.GetData = function () {
+   
+            var dataOBJ = null;
+        
 
+                console.log("Recuperation des donnees");
+            
+            var req = {
+ method: 'GET',
+ url: 'http://destock.u-strasbg.fr:8080/Destock/promo/liste',
+ headers: {
+   'Content-Type': 'application/x-www-form-urlencoded'
+ }
+};
 
-
-/*
-
-// Here we use Promises then
-// GET /accounts
-baseAccounts.getList().then(function (accounts) {
-  // Here we can continue fetching the tree :).
-
-  var firstAccount = accounts[0];
-  // This will query /accounts/123/buildings considering 123 is the id of the firstAccount
-  $scope.buildings = firstAccount.getList("buildings");
-
-  // GET /accounts/123/places?query=param with request header: x-user:mgonto
-  $scope.loggedInPlaces = firstAccount.getList("places", {query: param}, {'x-user': 'mgonto'})
-
-  // This is a regular JS object, we can change anything we want :)
-  firstAccount.name = "Gonto"
-
-  // If we wanted to keep the original as it is, we can copy it to a new element
-  var editFirstAccount = Restangular.copy(firstAccount);
-  editFirstAccount.name = "New Name";
-
-
-  // PUT /accounts/123. The name of this account will be changed from now on
-  firstAccount.put();
-  editFirstAccount.put();
-
-  // PUT /accounts/123. Save will do POST or PUT accordingly
-  firstAccount.save();
-
-  // DELETE /accounts/123 We don't have first account anymore :(
-  firstAccount.remove();
-
-  var myBuilding = {
-    name: "Gonto's Building",
-    place: "Argentina"
-  };
-
-  // POST /accounts/123/buildings with MyBuilding information
-  firstAccount.post("Buildings", myBuilding).then(function() {
-    console.log("Object saved OK");
-  }, function() {
-    console.log("There was an error saving");
+$http(req)
+            
+            
+            
+            
+            .success(function (data, status, headers, config) {
+                console.log("Donnes recues par le serveur : " + JSON.stringify(data) );
+                
+                        $scope.Resultat = data;
+                
+                
+                
+            })
+            .error(function (data, status, header, config) {
+                console.log("ERREUR : " + JSON.stringify(data) + JSON.stringify(status) );
+            });
+                  
+        };
+        
+     
   });
-
-*/
-
-
-
-
-  });
-
+  
+  
