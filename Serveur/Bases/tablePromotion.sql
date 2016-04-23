71bcad77a6c9091a722ce7e52932f0726b122fcc
boblebol <alexandre.enouf@gmail.com>
2016-04-23 14:40:58 +0200

    Bases
    Transformation de la description du produit du type blop au type varchar

--- Serveur/Bases/tablePromotion.sql
+++ Serveur/Bases/tablePromotion.sql
@@ -8,10 +8,9 @@ CREATE TABLE Promotions
 	 marquePromo        VARCHAR(50), -- marque du produit
 	 nomProduit			VARCHAR(50), -- marque du produit
 	 categorie       	     VARCHAR(50), -- catégorie du produit -> les imposer dans le formulaire d'ajout de promo
-	 description     	       BLOB, -- description du produit
+	 description     	       VARCHAR(100), -- description du produit
  	 ancienPrix      	    FLOAT, -- ancien prix du produit
 	 nouveauPrix      FLOAT, -- nouveau prix du produit
-	 --dateCreation     TIMESTAMP, -- date ou la promo a ete postee
 	 dureeValidite   	       INT,
 	 -- STATUT
 	 active			BOOLEAN, -- Tant que la promo as moins de 24h par defaut ou que 3 personne l'aient signalee
